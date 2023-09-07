ca93ef11327c01b503a66611cd24199ef439ae01
顏淩育 <louis@conquers.co>
2023-09-07 10:04:59 +0800

    debug docker running

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.create import create_router
-from routes.read import read_router
-from routes.update import update_router
+# from routes.create import create_router
+# from routes.read import read_router
+# from routes.update import update_router
+from routes import create, read, update, delete
 
 app = FastAPI()
+read_router = read.read_router
+create_router = create.create_router
+update_router = update.update_router
 
 app.debug = True
 origins = [
