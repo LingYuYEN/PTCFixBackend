222da6b9539eedc54bbb339b064b4c5d2854eb0a
顏淩育 <louis@conquers.co>
2023-09-28 09:44:55 +0800

    create SSL

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.create import create_router
 from routes.read import read_router
 from routes.update import update_router
-from config.ssl import generate_certificate
+# from config.ssl import generate_certificate
 app = FastAPI()
 
 app.debug = True
@@ -25,7 +25,7 @@ app.include_router(read_router, tags=['GET'])
 app.include_router(create_router, tags=['POST'])
 app.include_router(update_router, tags=['PUT'])
 
-generate_certificate()
+# generate_certificate()
 
 if __name__ == '__main__':
     # uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
