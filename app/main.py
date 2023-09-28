39a7795c9701d4edb28957206bbf28ffb85480b8
顏淩育 <louis@conquers.co>
2023-09-28 09:37:47 +0800

    create SSL

--- app/main.py
+++ app/main.py
@@ -28,4 +28,5 @@ app.include_router(update_router, tags=['PUT'])
 generate_certificate()
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
+    # uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
+    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, workers=1)
