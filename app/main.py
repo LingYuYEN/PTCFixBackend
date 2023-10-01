053c3635837e8c630d438ff43b05111808cfcdd7
顏淩育 <louis@conquers.co>
2023-10-01 23:31:41 +0800

    create SSL

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.create import create_router
 from routes.read import read_router
 from routes.update import update_router
-# from config.ssl import generate_certificate
+from config.ssl import generate_certificate
 app = FastAPI()
 
 app.debug = True
@@ -12,21 +12,20 @@ app.debug = True
 origins = [
     "*"
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(read_router, tags=['GET'])
 app.include_router(create_router, tags=['POST'])
 app.include_router(update_router, tags=['PUT'])
 
-# generate_certificate()
+generate_certificate()
 
 if __name__ == '__main__':
-    # uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
-    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, workers=1)
+    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
+    # uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, workers=1)
