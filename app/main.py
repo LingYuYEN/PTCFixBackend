8d7bb6b3e8ddb02f18beae0c6df1bd51f493d8bf
顏淩育 <louis@conquers.co>
2023-10-11 16:02:11 +0800

    create SSL

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import read, create, update
-from config.ssl import generate_certificate
+# from routes import read, create, update
+# from config.ssl import generate_certificate
+from config.db import db
+from models.model import Signin
 
 app = FastAPI()
 
@@ -22,34 +24,22 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# app.add_middleware(
-#     HTTPSRedirectMiddleware
-# )
+# app.include_router(read.read_router, tags=['GET'])
+# app.include_router(create.create_router, tags=['POST'])
+# app.include_router(update.update_router, tags=['PUT'])
+
+# generate_certificate()
 
 
-# @app.route("/")
-# async def index():
-#     return JSONResponse({"message": "success"}, status_code=200)
-#
-#
-# @app.route("/", methods=['OPTIONS'])
-# async def options():
-#     return JSONResponse(
-#         {},
-#         headers={
-#             'Allow': 'GET, POST, OPTIONS',
-#             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-#             'Access-Control-Allow-Origin': '*',
-#             'Access-Control-Allow-Headers': '*'
-#         }
-#     )
-
-app.include_router(read.read_router, tags=['GET'])
-app.include_router(create.create_router, tags=['POST'])
-app.include_router(update.update_router, tags=['PUT'])
-
-generate_certificate()
-
+@app.post("/signin")
+async def create_signin(signin: Signin):
+    collection = db['members']
+    myquery = {'account': signin.account}
+    # myquery = {'account': 'yuxp0130@gmail.com'}
+    myCol = collection.find_one(myquery, {'_id': 0})
+    result = False if myCol is None else True
+    print(result)
+    return {"status": "Ok", "data": result}
 
 if __name__ == '__main__':
     # uvicorn.run('main:app', host="0.0.0.0", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
