5262896fa1a08b9b07523416a3407ea99d7a0eed
顏淩育 <louis@conquers.co>
2023-10-16 13:02:10 +0800

    modify docker file path

--- app/main.py
+++ app/main.py
@@ -31,6 +31,6 @@ app.include_router(update.update_router, tags=['PUT'])
 
 # generate_certificate()
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # uvicorn.run('main:app', host="0.0.0.0", port=5000, log_level="info", reload=True, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem", workers=1)
-    uvicorn.run('main:app', host="127.0.0.1", port=5000, log_level="info", reload=True, workers=1)
+    # uvicorn.run('main:app', host="0.0.0.0", port=5000, log_level="info", reload=True, workers=1)
